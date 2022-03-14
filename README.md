d511f44953fbe97d223642f26db3aa4fe8bc6072
ruoan <ruoan777@gmail.com>
2022-03-14 22:11:52 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ruoandebug
-ruoandebug
+# ruoan-framework
+记录了一些平时工作中用到到一些组件
--- web/src/main/java/com/ustc/ruoan/framework/web/service/RedisService.java
+++ web/src/main/java/com/ustc/ruoan/framework/web/service/RedisService.java
@@ -3,6 +3,7 @@ package com.ustc.ruoan.framework.web.service;
 import com.ustc.ruoan.framework.redis.ano.RedisProvider;
 import com.ustc.ruoan.framework.redis.provider.CacheProvider;
 import com.ustc.ruoan.framework.web.anno.MethodLog;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -21,6 +22,7 @@ public class RedisService {
 
     @MethodLog
     public String anotherGet(String key) throws InterruptedException {
+        RedisService redisService = (RedisService)AopContext.currentProxy();
         return cacheProvider.get(key);
     }
 }
